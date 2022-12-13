893742863bc02eb2ad5843d030568f76665a352b
subin <whtnqlsm1004@gmail.com>
2022-12-13 18:34:04 +0900

    컴포넌트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/header/Header";
+import TodoList from "./components/todoList/TodoList";
 
 function App() {
-  return <div></div>;
+  const [filter, setFilter] = useState("All");
+
+  return (
+    <>
+      <Header filter={filter} changeFilter={setFilter} />
+      <TodoList filter={filter} />
+    </>
+  );
 }
 
 export default App;
